975ede739cecbe5625855145333c77dcdcbff726
Kristoffer <kristofferhumle@gmail.com>
2023-12-30 20:41:57 +0400

    Updating naming

---
+++ sun/lamp.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+
+PIN = 14
+
+class Lamp():
+    def __init__(self):
+        GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(PIN, GPIO.OUT)
+        GPIO.output(PIN, GPIO.HIGH)
+        self.current = GPIO.HIGH
+        
+    def light(self):
+        if self.current == GPIO.LOW:
+            return
+       
+        GPIO.output(PIN, GPIO.LOW)
+        self.current = GPIO.LOW
+
+    def dark(self):
+        if self.current == GPIO.HIGH:
+            return
+        
+        GPIO.output(PIN, GPIO.HIGH)
+        self.current = GPIO.HIGH
+       
+    def shutdown(self):
+        GPIO.cleanup()
---
+++ sun/main.py
@@ -0,0 +1,43 @@
+import rclpy
+from . import lamp
+from rclpy.node import Node
+from std_msgs.msg import String
+
+DAY = 'day'
+NIGHT = 'night'
+
+class Sun(Node):
+
+    def __init__(self):
+        super().__init__('sun')
+        self.lamp = lamp.Lamp()
+        self.subscription = self.create_subscription(
+            String,
+            'day',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+
+        self.get_logger().info('Getting: "%s"' % msg.data)
+        
+        if msg.data == DAY:
+            self.lamp.light()
+        
+        elif msg.data == NIGHT:
+            self.lamp.dark()
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    sun_subscriber = Sun()
+
+    rclpy.spin(sun_subscriber)
+
+    sun_subscriber.sun.shutdown()
+    sun_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
