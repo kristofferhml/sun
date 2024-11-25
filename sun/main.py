72d739a848f633e0e1a16d455102dc9cd29d92b2
kristoffer <kristoffer@teletronics.ae>
2024-11-25 20:26:16 +0400

    Make PIN config and dont cleanup in beginning

--- sun/main.py
+++ sun/main.py
@@ -1,16 +1,18 @@
 import rclpy
+import os
 from . import lamp
 from rclpy.node import Node
 from std_msgs.msg import String
 
 DAY = 'day'
 NIGHT = 'night'
+PIN = int(os.getenv('PIN',14))
 
 class Sun(Node):
 
     def __init__(self):
         super().__init__('sun')
-        self.lamp = lamp.Lamp()
+        self.lamp = lamp.Lamp(PIN)
         self.subscription = self.create_subscription(
             String,
             'day',
